1279f0b5c0680bf291dc0a3e70b9564e648028ec
Kairos <danielroepke01@gmail.com>
2025-03-14 13:29:13 +0100

    Ready? Set. Go!

---
+++ README.md
@@ -0,0 +1,27 @@
+- Credit system
+    - Users can gain credits by participating in events/commands
+    - Users can spend credits on items
+    - Shop System
+- funny useless commands
+    - `!8ball`
+    - `!leave wafflehouse` : `You can never leave the wafflehouse`
+    - `!make waffle` : `You made a waffle! + 2 credits`
+- Admin commands
+    - Add credits to a user
+    - Remove credits from a user
+    - Add items to the shop
+    - Remove items from the shop
+    - Add items to a user
+    - Remove items from a user
+- Random encounters
+  - Some random encounters can lead to fights depending on whether the user accepts or not
+  - Some random encounters just result in nothing
+  - Some random encounters can result in good fortune
+  - Some random encounters you get an option (Dodge - Catch, if you dodge you get nothing, if you catch theres a chance you lose and a chance you win)
+
+Fights:
+- When you get challenged to a fight you get a couple of options
+    - Accept
+    - Ignore
+    - Get the manager (Gets Swig, Shado, or Trip and gets some dialogue resulting in either nothing, more credits or less credits)
+    - I am the manager (Something like an intimidation check)
---
+++ index.js
@@ -0,0 +1,45 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const dotenv = require('dotenv');
+dotenv.config();
+const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+
+
+// Constants
+const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
+
+
+// Setup
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+    ]
+});
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// Set a new item in the Collection with the key as the command name and the value as the exported module
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+client.once(Events.ClientReady, readyClient => {
+	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+});
+
+// Log in to Discord with your client's token
+client.login(DISCORD_TOKEN);
