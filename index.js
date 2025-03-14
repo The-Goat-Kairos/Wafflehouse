da4e3c0183c5b34cc831b762baa7421138829ee1
Kairos <danielroepke01@gmail.com>
2025-03-14 14:43:08 +0100

    Fixed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const fs = require('node:fs');
 const path = require('node:path');
 const dotenv = require('dotenv');
 dotenv.config();
-const { Client, Events, Collection, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 
 
 // Constants
@@ -35,6 +35,21 @@ for (const folder of commandFolders) {
 			console.log(`[WARNING]: The command at ${filePath} is missing a required "data" or "execute" property.`);
 		}
 	}
+}
+
+
+// Load all event files
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args, client));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args, client));
+    }
 }
 
 
