1712d67e2f3fc15a71746c34849691ab173fc5a6
Kairos <danielroepke01@gmail.com>
2025-05-06 14:50:24 +0200

    Basic framework for optionMessages. I don't know if this way is smart or not but I'll deal with the consequences later lol

--- handlers/optionEvents.js
+++ handlers/optionEvents.js
@@ -1,11 +1,58 @@
 class OptionEvent {
-    constructor() {
+    constructor(initialMessage, optionOne, optionTwo) {
+        this.initialMessage = initialMessage;
+        this.optionOne = optionOne
+        this.optionTwo = optionTwo
+    }
 
+    get initialMessage() {
+        return this.initialMessage;
+    }
+
+    get optionOne() {
+        return this.optionOne;
+    }
+
+    get optionTwo() {
+        return this.optionTwo;
     }
 }
 
+class Option {
+    constructor(optionName, values) {
+        this.optionName = optionName;
+        this.values = values;
+    }
+
+    getRandomValue() {
+        return this.values[Math.floor(Math.random() * this.values.length)];
+   }
+
+    get optionName() {
+        return this.optionName;
+    }
+}
+
+// Option
+// - OptionName: String
+// - [
+//      {potentialMessage: String, potentialCost: Int, weight: Float (Percentage)},
+//      ...,
+//      ...
+//   ]
+
 const optionEvents = [
-    new OptionEvent(),
+    new OptionEvent(
+        "A drunk man throws a beer at you!",
+        new Option("Dodge", [
+            {message: "You dodge the bottle!", gain: 0, weight: 0.5},
+            {message: "You trip and the bottle smashes you in the face", gain: -10, weight: 0.5}
+        ]),
+        new Option("Catch", [
+            {message: "You do a sick flip and catch the bottle", gain: 10, weight: 0.3},
+            {message: "The bottle smashes cleanly into your face", gain: -5, weight: 0.7}
+        ]),
+    )
 ];
 
 module.exports = optionEvents;
