e0689ae2afbfa251cd1821996109ca161a638255
Kairos <danielroepke01@gmail.com>
2025-05-08 19:24:31 +0200

    Nerfed it. Not sure about these values but ya gotta do what swig tells you ya gotta do

--- handlers/optionEvents.js
+++ handlers/optionEvents.js
@@ -69,12 +69,11 @@ const optionEvents = [
         [
             new Option("Dodge", [
                 new OptionResult("You dodge the bottle!", 0, 0.75),
-                new OptionResult("You dodge the bottle and stumble into a good waffle. Delicious!", 10, 0.1),
-                new OptionResult("You trip and the bottle smashes you in the face", -10, 0.15)
+                new OptionResult("You try to dodge, trip, and the bottle ends up shattering on the back of your head...", -10, 0.25)
             ]),
             new Option("Catch", [
-                new OptionResult("You do a sick flip and catch the bottle", 10, 0.3),
-                new OptionResult("The bottle smashes cleanly into your face", -5, 0.7)
+                new OptionResult("You do a sick flip and catch the bottle!", 20, 0.25),
+                new OptionResult("You try to catch the bottle, but it smashes cleanly into your face...", -5, 0.75)
             ]),
         ]
     ),
@@ -82,12 +81,12 @@ const optionEvents = [
         "You find a wallet on the ground.",
         [
             new Option("Return it to the owner", [
-                new OptionResult("You find the owner and return the wallet. They thank you!", 10, 0.7),
-                new OptionResult("The owner is rude and accuses you of stealing!", -5, 0.3)
+                new OptionResult("You find the owner and return the wallet. They thank you!", 10, 0.75),
+                new OptionResult("The owner is rude and accuses you of stealing!", -5, 0.25)
             ]),
             new Option("Keep it for yourself", [
-                new OptionResult("You find $40 inside!", 40, 0.2),
-                new OptionResult("You get caught by the police!", -20, 0.8)
+                new OptionResult("You find $40 inside!", 30, 0.25),
+                new OptionResult("You get caught by the police!", -15, 0.75)
             ])
         ]
     ),
@@ -95,13 +94,12 @@ const optionEvents = [
         "A mysterious stranger offers you a potion.",
         [
             new Option("Drink the potion", [
-                new OptionResult("You feel invigorated and gain some very needed energy.", 20, 0.2),
-                new OptionResult("Your hair falls out but it *did* cure your headache.", 8, 0.2),
-                new OptionResult("You feel sick and lose some health.", -15, 0.6)
+                new OptionResult("You feel invigorated and gain some very needed energy.", 25, 0.25),
+                new OptionResult("You feel sick and lose some health.", -15, 0.75)
             ]),
             new Option("Refuse the potion", [
-                new OptionResult("You walk away safely from what was most likely poison. Good job, you.", 0, 0.8),
-                new OptionResult("The stranger curses you!", -5, 0.2)
+                new OptionResult("You walk away safely from what was most likely poison. Good job, you.", 0, 0.75),
+                new OptionResult("The stranger curses you!", -5, 0.25)
             ])
         ]
     ),
