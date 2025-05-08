cab21c40d146ed5fbb5af978839ed1f8a671b078
Kairos <danielroepke01@gmail.com>
2025-05-08 19:19:29 +0200

    Option Events Added! I overengineered the shit out of this!!!!!

--- handlers/optionEvents.js
+++ handlers/optionEvents.js
@@ -1,38 +1,60 @@
 class OptionEvent {
-    constructor(initialMessage, optionOne, optionTwo) {
-        this.initialMessage = initialMessage;
-        this.optionOne = optionOne
-        this.optionTwo = optionTwo
+    #initialMessage;
+    #options;
+    constructor(initialMessage, options) {
+        this.#initialMessage = initialMessage;
+        this.#options = options
     }
 
     get initialMessage() {
-        return this.initialMessage;
+        return this.#initialMessage;
     }
 
-    get optionOne() {
-        return this.optionOne;
-    }
-
-    get optionTwo() {
-        return this.optionTwo;
+    get options() {
+        return this.#options;
     }
 }
 
 class Option {
+    #optionName;
+    #values;
     constructor(optionName, values) {
-        this.optionName = optionName;
-        this.values = values;
+        this.#optionName = optionName;
+        this.#values = values;
     }
 
-    getRandomValue() {
-        return this.values[Math.floor(Math.random() * this.values.length)];
-   }
+    getWeightedRandomValue() {
+        const totalWeight = this.#values.reduce((sum, value) => sum + value.weight, 0);
+        if (totalWeight !== 1) {
+            console.error(`You fucked up. The total weight of options for ${optionName()} is ${totalWeight}.`);
+        }
+        const random = Math.random() * totalWeight;
+        let cumulativeWeight = 0;
+
+        for (const value of this.#values) {
+            cumulativeWeight += value.weight;
+            if (random < cumulativeWeight) {
+                return value; // Return the selected value
+            }
+        }
+    }
+
+    get values() {
+        return this.#values;
+    }
 
     get optionName() {
-        return this.optionName;
+        return this.#optionName;
     }
 }
 
+class OptionResult {
+    constructor(message, gain, weight) {
+        this.message = message;
+        this.gain = gain;
+        this.weight = weight;
+    }
+}
 // Option
 // - OptionName: String
 // - [
@@ -44,15 +66,46 @@ class Option {
 const optionEvents = [
     new OptionEvent(
         "A drunk man throws a beer at you!",
-        new Option("Dodge", [
-            {message: "You dodge the bottle!", gain: 0, weight: 0.5},
-            {message: "You trip and the bottle smashes you in the face", gain: -10, weight: 0.5}
-        ]),
-        new Option("Catch", [
-            {message: "You do a sick flip and catch the bottle", gain: 10, weight: 0.3},
-            {message: "The bottle smashes cleanly into your face", gain: -5, weight: 0.7}
-        ]),
-    )
+        [
+            new Option("Dodge", [
+                new OptionResult("You dodge the bottle!", 0, 0.75),
+                new OptionResult("You dodge the bottle and stumble into a good waffle. Delicious!", 10, 0.1),
+                new OptionResult("You trip and the bottle smashes you in the face", -10, 0.15)
+            ]),
+            new Option("Catch", [
+                new OptionResult("You do a sick flip and catch the bottle", 10, 0.3),
+                new OptionResult("The bottle smashes cleanly into your face", -5, 0.7)
+            ]),
+        ]
+    ),
+    new OptionEvent(
+        "You find a wallet on the ground.",
+        [
+            new Option("Return it to the owner", [
+                new OptionResult("You find the owner and return the wallet. They thank you!", 10, 0.7),
+                new OptionResult("The owner is rude and accuses you of stealing!", -5, 0.3)
+            ]),
+            new Option("Keep it for yourself", [
+                new OptionResult("You find $40 inside!", 40, 0.2),
+                new OptionResult("You get caught by the police!", -20, 0.8)
+            ])
+        ]
+    ),
+    new OptionEvent(
+        "A mysterious stranger offers you a potion.",
+        [
+            new Option("Drink the potion", [
+                new OptionResult("You feel invigorated and gain some very needed energy.", 20, 0.2),
+                new OptionResult("Your hair falls out but it *did* cure your headache.", 8, 0.2),
+                new OptionResult("You feel sick and lose some health.", -15, 0.6)
+            ]),
+            new Option("Refuse the potion", [
+                new OptionResult("You walk away safely from what was most likely poison. Good job, you.", 0, 0.8),
+                new OptionResult("The stranger curses you!", -5, 0.2)
+            ])
+        ]
+    ),
 ];
+
 
 module.exports = optionEvents;
---
+++ handlers/testing.js
@@ -0,0 +1,20 @@
+const optionEvents = require("./optionEvents.js")
+
+function displayOptionEvents(events) {
+    events.forEach((event, index) => {
+        console.log(`Event ${index + 1}: ${event.initialMessage}`);
+        console.log("Options:");
+
+        event.options.forEach((option, optionIndex) => {
+            console.log(`  ${optionIndex + 1}. ${option.optionName}`);
+            option.values.forEach((result, resultIndex) => {
+                console.log(`     - Result ${resultIndex + 1}: ${result.message} (Gain: ${result.gain}, Weight: ${result.weight})`);
+            });
+        });
+
+        console.log(""); // Add a blank line for better readability
+    });
+}
+
+// Call the function to display the events
+displayOptionEvents(optionEvents);
